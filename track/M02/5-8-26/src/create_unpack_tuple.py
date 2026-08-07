414a1d0c09d735aadb61d347226fc4d3f1253973
Ranjith950 <rk90142495@gmail.com>
2026-08-07 15:45:30 +0530

    done

---
+++ track/M02/5-8-26/src/create_unpack_tuple.py
@@ -0,0 +1,9 @@
+name=input("enter the name:")
+course=input("enter the course:")
+score=int(input("enter the score:"))
+tuple=(name,course,score)
+print(tuple)
+name,course,score=tuple
+print("name=",name)
+print("course=",course)
+print("score=",score)
