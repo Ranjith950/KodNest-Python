f6687ac7caf8dd5f5051f394b8752ec6329a7627
Ranjith950 <rk90142495@gmail.com>
2026-08-07 16:11:42 +0530

    work

---
+++ track/M02/5-8-26/src/tuple_without_modify.py
@@ -0,0 +1,7 @@
+course_name=input("enter the course name:")
+current_week=input("enter the current week:")
+course_status=input("enter the course status:-")
+tuple=(course_name,current_week,course_status)
+updated_week=input("enter the updated week:- ")
+tuple=(course_name,updated_week,course_status)
+print(tuple)
