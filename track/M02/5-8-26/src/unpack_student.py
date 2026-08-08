41d54b1972dd8c1567b264257c3cb3a6e66d0f50
Ranjith950 <rk90142495@gmail.com>
2026-08-08 23:03:03 +0530

    done

---
+++ track/M02/5-8-26/src/unpack_student.py
@@ -0,0 +1,8 @@
+name=input("enter the name:")
+course=input("enter the course")
+score=int(input("enter the score"))
+student_record=(name,course,score)
+name1,course,score=student_record
+print(f"name is:{name1}")
+print(f"course is:{course}")
+print(f"score is:{score}")
