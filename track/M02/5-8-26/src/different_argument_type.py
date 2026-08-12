94d26f493b80bf858fc04569d007f3d7e35a10ac
Ranjith950 <rk90142495@gmail.com>
2026-08-12 08:32:24 +0530

    display student

---
+++ track/M02/5-8-26/src/different_argument_type.py
@@ -0,0 +1,6 @@
+def display_student(name,course,level):
+    print(f"{name} | {course} | {level}")
+
+display_student("vishnu","python","Beginner")
+display_student(name="teja",course="java",level="Intermediate")
+display_student("madhu",course="ml",level="Advanced")
